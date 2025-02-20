3939d87f53a4374554b5b973d442a1a65f7bf187
raheelnasir <iraheelnasir@gmail.com>
2025-02-20 16:27:19 +0500

    feature(storage): add router

--- src/router/files.route.ts
+++ src/router/files.route.ts
@@ -1,12 +1,31 @@
 import express from "express";
-import { deleteFile, getFileMetadata, getFileUrl, uploadFile } from "../controller";
+import { uploadFile, getFileUrl, getFileMetadata, deleteFile } from "../controller";
 import { upload } from "../config";
 
 const router = express.Router();
 
+/**
+ * @route POST /upload
+ * @description Upload a file to Appwrite
+ */
 router.post("/upload", upload.single("file"), uploadFile);
+
+/**
+ * @route GET /file/:fileId
+ * @description Retrieve file preview URL
+ */
 router.get("/file/:fileId", getFileUrl);
+
+/**
+ * @route GET /file/metadata/:fileId
+ * @description Retrieve file metadata
+ */
 router.get("/file/metadata/:fileId", getFileMetadata);
+
+/**
+ * @route DELETE /file/:fileId
+ * @description Delete a file from Appwrite
+ */
 router.delete("/file/:fileId", deleteFile);
 
 export default router;
